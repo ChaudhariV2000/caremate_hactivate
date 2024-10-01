a5d53efe8a7e14b9f793e82583950930773414ac
ChaudhariV2000 <ChaudhariV2000>
2024-10-01 10:52:12 +0530

    final commit

--- Client/src/Pages/Room.jsx
+++ Client/src/Pages/Room.jsx
@@ -3,19 +3,19 @@ import { useParams } from 'react-router-dom'
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 const Room = () => {
-  const {roomId} = useParams();
+  const { roomId } = useParams();
 
   const myMeeting = async (element) => {
     const appID = 1122569529;
     const ServerSecret = "c419d589f3561e8a702006e9ca297130";
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID,ServerSecret,roomId,Date.now().toString(),"Ocean");
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, ServerSecret, roomId, Date.now().toString(), "Ocean");
     const zc = ZegoUIKitPrebuilt.create(kitToken);
     zc.joinRoom({
       container: element,
-      sharedLinks:[ 
+      sharedLinks: [
         {
-            name: "Copy Link",
-            url: `http://localhost:5137/room/${roomId}`,
+          name: "Copy Link",
+          url: `http://localhost:5173/room/${roomId}`,
         }
       ],
       scenario: {
@@ -25,9 +25,9 @@ const Room = () => {
   };
 
   return (
-      <div className='mt-2 mb-5'>
-        <div ref={myMeeting}/>
-      </div>
+    <div className='mt-2 mb-5'>
+      <div ref={myMeeting} />
+    </div>
   )
 }
 
