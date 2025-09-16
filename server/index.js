4adbb9cd3b539e07e21a8947a573662614f98762
Vedant Chaudhari <125193317+ChaudhariV2000@users.noreply.github.com>
2025-09-16 17:12:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,50 +27,50 @@ app.use("/", require("./Routes/authroutes"));
 
 //--------
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
-const authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
-const twilioClient = twilio(accountSid, authToken);
+// const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+// const authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
+// const twilioClient = twilio(accountSid, authToken);
 
-// Twilio phone number
-const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+// // Twilio phone number
+// const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
-cron.schedule('* * * * *', async () => {
-  const now = new Date();
-  const reminders = await Reminder.find({
-    date: { $lte: now },
-    completed: false,
-  });
+// cron.schedule('* * * * *', async () => {
+//   const now = new Date();
+//   const reminders = await Reminder.find({
+//     date: { $lte: now },
+//     completed: false,
+//   });
 
-  for (const reminder of reminders) {
-    console.log(`Reminder: ${reminder.title} - Time: ${reminder.time}`);
+//   for (const reminder of reminders) {
+//     console.log(`Reminder: ${reminder.title} - Time: ${reminder.time}`);
 
 
-    try {
-      await twilioClient.messages.create({
-        body: `Reminder: ${reminder.title} - Time: ${reminder.time}`,
-        from: twilioPhoneNumber,
-        to: "+919860173150" // Replace with actual user phone number from the reminder
-      });
+//     try {
+//       await twilioClient.messages.create({
+//         body: `Reminder: ${reminder.title} - Time: ${reminder.time}`,
+//         from: twilioPhoneNumber,
+//         to: "+919860173150" // Replace with actual user phone number from the reminder
+//       });
 
-      console.log(`SMS sent to ${reminder.userPhoneNumber}`);
-    } catch (error) {
-      console.error('Error sending SMS:', error);
-    }
-    try {
-      const response = await axios.post('https://0875-27-0-59-131.ngrok-free.app/call-user', {
-        phone: reminder.phoneNumber,
+//       console.log(`SMS sent to ${reminder.userPhoneNumber}`);
+//     } catch (error) {
+//       console.error('Error sending SMS:', error);
+//     }
+//     try {
+//       const response = await axios.post('https://0875-27-0-59-131.ngrok-free.app/call-user', {
+//         phone: reminder.phoneNumber,
 
-      });
+//       });
 
-      console.log(`Route /user-route called successfully for ${reminder.userPhoneNumber}. Response:`, response.data);
-    } catch (error) {
-      console.error('Error calling user route:', error);
-    }
+//       console.log(`Route /user-route called successfully for ${reminder.userPhoneNumber}. Response:`, response.data);
+//     } catch (error) {
+//       console.error('Error calling user route:', error);
+//     }
 
-    reminder.completed = true;
-    await reminder.save();
-  }
-});
+//     reminder.completed = true;
+//     await reminder.save();
+//   }
+// });
 
 
 
