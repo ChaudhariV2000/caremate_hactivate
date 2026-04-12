d82e640367a129d92a41fec32afb6847ce5c37d0
Vedant Chaudhari <125193317+ChaudhariV2000@users.noreply.github.com>
2026-04-13 00:01:01 +0530

    Update appID and serverSecret values in Room.jsx

--- Client/src/Pages/Room.jsx
+++ Client/src/Pages/Room.jsx
@@ -6,8 +6,8 @@ const Room = () => {
   const { roomId } = useParams();
 
   const myMeeting = async (element) => {
-    const appID =1965630916;//1668608289;//1568069075;// 739996763;// 1122569529;
-    const serverSecret ="cddab2a9363398bee0621095de8af08c";//"f2516865b7bbf3c8906d230bf4294bcb";//"960c273d1f368c9f3f0451d485f01858";//"dce929ef61dd0e7156086609cfd98a27";//"c419d589f3561e8a702006e9ca297130";
+    const appID =1954584908;//1965630916;//1668608289;//1568069075;// 739996763;// 1122569529;
+    const serverSecret ="cfafba2e877428f1448cd2566e974501";//"cddab2a9363398bee0621095de8af08c";//"f2516865b7bbf3c8906d230bf4294bcb";//"960c273d1f368c9f3f0451d485f01858";//"dce929ef61dd0e7156086609cfd98a27";//"c419d589f3561e8a702006e9ca297130";
     const userID = Date.now().toString();
     const userName = "Vedant";
 
